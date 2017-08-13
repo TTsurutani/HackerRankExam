900f3b748d99edccd6231e28eb655b685f6221ba
TTsurutani <tbird@mbj.nifty.com>
2017-08-14 05:41:08 +0900

    途中経過

--- MissingNumbers.hs
+++ MissingNumbers.hs
@@ -2,7 +2,18 @@ import Data.List
 main :: IO()
 main = do
     [_ , xs , _ , ys ] <- map words.lines <$> getContents
-    print $ nub xs
-    print $ nub ys
+    print  xs
+    print  ys
+
+elements ::Eq a => a -> [a] -> Int
+elements _ [] = 0
+elements x (y:ys)
+       | x == y    = elements x ys + 1
+       | otherwise = elements x ys
+
+counts :: [a] -> [(a,Int)]
+counts [] = []
+
+
 
 -- type MissingNumbers.txt | stack runghc MissingNumbers.hs    
