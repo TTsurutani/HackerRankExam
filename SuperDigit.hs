a8e87328fc51053094073a4834f260b56e09ca0a
tbird <tbird@jp.ibm.com>
2017-08-18 14:06:02 +0900

    入力を一回足しこむところまで完成

--- SuperDigit.hs
+++ SuperDigit.hs
@@ -1,19 +1,26 @@
+import Data.Char
 main::IO()
 main = do 
     [a,n] <- words <$> getContents
+    -- a = "148" n = "3"
     let x = concat $ replicate (stringToInt n) a
-    print x
+    -- x = "148148148"
     print $ sumString x
 
 stringToInt :: String -> Int
 stringToInt = read::String->Int
-stringToInteger :: String -> Integer
-stringToInteger = read::String->Integer
+
+charToInt :: Char -> Int
+charToInt x = ord x - 48
 
 sumString :: String -> Integer
-sumString = foldr ((+) . stringToInteger) 0
+sumString = foldr ((+) . toInteger . charToInt) 0
 
+{--
+sumString [] = 0
+sumString (x:xs) = toInteger (charToInt x) + sumString xs
 
 -- type SuperDigit.txt | stack runghc SuperDigit.hs
 -- https://www.hackerrank.com/challenges/super-digit
 
+ --}
