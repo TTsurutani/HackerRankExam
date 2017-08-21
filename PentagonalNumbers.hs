4824c921dae95e5f52922717f169246b1b34739c
tbird <tbird@jp.ibm.com>
2017-08-21 14:25:15 +0900

    一部タイムアウトするロジック

--- PentagonalNumbers.hs
+++ PentagonalNumbers.hs
@@ -1,15 +1,22 @@
--- |
--- >>> penta 1
--- 1
--- >>> penta 2
--- 5
--- >>> penta 3
--- 12
--- >>> penta 4
--- 22
--- >>> penta 5
--- 35
+main :: IO()
+main = do
+    _ : xs <- lines <$> getContents
+    -- ["1","2","3","4","5"]
+    mapM_ (print . penta . (read :: String -> Int)) xs
 
--- type input\PentagonalNumbers.txt | PentagonalNumbers.hs
+-- 頂点の数 n = 頂点の数(n-1) + (n-1)*5
+-- 重複するdotの数 = n=1,2 zero n>3 n + n*(n-3)
 
--- https://www.hackerrank.com/challenges/pentagonal-numbers?
+penta :: Int -> Int
+penta 1 = 1
+penta 2 = 5
+penta n = summit (n - 1) + (n - 1)*5 - (n + n*(n-3))
+
+summit :: Int -> Int
+summit 1 = 1
+summit 2 = 5
+summit n = summit (n - 1) + 5*(n - 1)
+
+-- type input\PentagonalNumbers.txt | stack runghc PentagonalNumbers.hs
+
+-- https://www.hackerrank.com/challenges/pentagonal-numbers
