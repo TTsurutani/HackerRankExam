7912e81089fc0e3a610163f628ab9e6f7b03a04d
tbird <tbird@jp.ibm.com>
2017-08-21 10:05:43 +0900

    [[String]]に分解まで

--- ListsAndGCD.hs
+++ ListsAndGCD.hs
@@ -1,2 +1,9 @@
--- type input\ListsAndGCD.txt | ListsAndGCD.hs
+
+main :: IO()
+main = do
+    _ : list <- lines <$> getContents
+    print $ map words list
+
+
+-- type input\ListsAndGCD.txt | stack runghc ListsAndGCD.hs
 -- https://www.hackerrank.com/challenges/lists-and-gcd
