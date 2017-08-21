c6bd5c7b205212183247d39de7cae5ea5de8bbf9
tbird <tbird@jp.ibm.com>
2017-08-21 10:20:31 +0900

    素数の累乗のペアまで

--- ListsAndGCD.hs
+++ ListsAndGCD.hs
@@ -2,8 +2,18 @@
 main :: IO()
 main = do
     _ : list <- lines <$> getContents
-    print $ map words list
+    let tlist = map words list
+    -- [["2","2","3","2","5","3"],["3","2","5","3","11","1"]]
+    let plist = map pairs tlist
+    -- [[(2,2),(3,2),(5,3)],[(3,2),(5,3),(11,1)]]
+    print $ plist
 
+pairs :: [String] -> [(Int,Int)]
+pairs [] = []
+pairs (x:y:xs) = (read x, read y) : pairs xs
+
+depairs :: [(Int,Int)] -> String
+depairs = undefined
 
 -- type input\ListsAndGCD.txt | stack runghc ListsAndGCD.hs
 -- https://www.hackerrank.com/challenges/lists-and-gcd
