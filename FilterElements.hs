131091d89c0d552c1b586dd07077bbfc454d3bac
tbird <tbird@jp.ibm.com>
2017-08-22 12:52:38 +0900

    解法へのアイデアを記入

--- FilterElements.hs
+++ FilterElements.hs
@@ -9,3 +9,18 @@ main = do
 -- https://www.hackerrank.com/challenges/filter-elements
 
 -- filter (>2) $ nub [4, 5, 2, 5, 4, 3, 1, 3, 4]
+{-- 
+Sample Input
+3                   :test case count
+9 2                 :list size & repetition count
+4 5 2 5 4 3 1 3 4   :list (4*2 5*2 2*1 3*2 1*1)
+9 4
+4 5 2 5 4 3 1 3 4
+10 2
+5 4 3 2 1 1 2 3 4 5 :list (5*2 4*2 3*2 2*2 1*2)
+
+Sample Output
+4 5 3
+-1
+5 4 3 2 1
+--}
--- FindDigits.hs
+++ FindDigits.hs
@@ -1,6 +1,18 @@
-import Control.Monad
+main :: IO()
+main = do
+    _ : list <- lines <$> getContents
+    -- ["12","1012"]
+    print list
 
-main ::IO()
+-- 数字文字列を受けて、条件に合致する数を返す
+--　数字を数値に
+--　数字をばらしながら数値に
+--　ばらした数値のリストをnub
+-- nubした数値でリストをカウント
+--　nubした数値で元数字を割って対象か否かを判定、Trueなら積算
+
+solve :: String -> Int
+solve = undefined
 
 
 -- type input\FindDigits.txt | stack runghc FindDigits.hs
