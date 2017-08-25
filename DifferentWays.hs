9b24bc293890b4c3321fdace9a1122f825125b1f
tbird <tbird@jp.ibm.com>
2017-08-25 16:22:35 +0900

    メモ化を試行したが、うまくいっていない

--- DifferentWays.hs
+++ DifferentWays.hs
@@ -1,13 +1,19 @@
+import Data.Function.Memoize
+
 main :: IO()
 main = do
     _:list <- lines <$> getContents
     print $ map words list
 
+countMemo :: Int -> Int -> Int
+countMemo = memoize count    
+
+
 count :: Int -> Int -> Int
 count n k 
    | k == 0 = 1
    | k == n  = 1
-   | otherwise = count (n -1) (k - 1) + count (n -1) k
+   | otherwise = countMemo (n -1) (k - 1) + countMemo (n -1) k
 
 -- https://www.hackerrank.com/challenges/different-ways-fp
 -- type input\DifferentWays.txt | stack runghc DifferentWays.hs
