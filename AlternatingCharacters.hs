f36507da51a4c6ab438aea3898032f35bf955a12
TTsurutani <tbird@mbj.nifty.com>
2017-08-27 18:11:10 +0900

    AlternatingCharacters一応解けた

--- AlternatingCharacters.hs
+++ AlternatingCharacters.hs
@@ -1,9 +1,16 @@
+main :: IO()
+main = do
+    _:list <- lines <$> getContents
+    mapM_ print $ map solve list
 
+solve :: String -> Int
+solve xs = (length xs) - (length $ del xs)
 
-
-
-
-
+del :: String -> String
+del [x] = [x]
+del (x:y:xs)
+    | x == y    = del (y:xs)
+    | otherwise = x : del (y:xs) 
 
 -- type input\AlternatingCharacters.txt | stack runghc AlternatingCharacters.hs
 -- https://www.hackerrank.com/challenges/alternating-characters
