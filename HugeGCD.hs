1bb5a5a2f9af9739b7a384df53b9847302cf9a57
TTsurutani <tbird@mbj.nifty.com>
2017-09-09 15:38:29 +0900

    大きい数字が扱えないらしい

--- HugeGCD.hs
+++ HugeGCD.hs
@@ -1,8 +1,17 @@
 main :: IO()
 main = do
-   [ _ ,a,_,b] <- map (words.(read::String->Int)) . lines <$> getContents
-   print a
-   print b
+  -- [ _ ,a,_,b] <- map (read::String->Integer) $ map words . lines <$> getContents
+  [ _ ,a,_,b] <- map words . lines <$> getContents
+  let x =  product $ map (read::String->Integer) a
+  let y =  product $ map (read::String->Integer) b
+  print $ gcd x y
+ 
+mygcd :: Integer ->Integer -> Integer
+mygcd a 0 = a
+mygcd a b
+   | b > a     = mygcd b a
+   | otherwise = mygcd b c
+         where c  = a `mod` b
 
 -- https://www.hackerrank.com/challenges/huge-gcd-fp   
 -- type input\HugeGCD.txt | stack runghc HugeGCD.hs
