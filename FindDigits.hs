31786bb763d7de030b5abcc1abc4035a8d9c75f7
TTsurutani <tbird@mbj.nifty.com>
2017-09-19 21:04:43 +0900

    割られる方と割る方に分離するところまで

--- FindDigits.hs
+++ FindDigits.hs
@@ -1,8 +1,13 @@
+import Data.Char
+import Data.List
 main :: IO()
 main = do
     _ : list <- lines <$> getContents
     -- ["12","1012"]
-    print $ map words list
+    let x1 = map (read::String->Int) list
+    let x2 = map (nub.sep) list
+    print x1
+    print x2
 
 -- 数字文字列を受けて、条件に合致する数を返す
 --　数字を数値に
@@ -11,8 +16,9 @@ main = do
 -- nubした数値でリストをカウント
 --　nubした数値で元数字を割って対象か否かを判定、Trueなら積算
 
-solve :: String -> Int
-solve = undefined
+sep :: String -> [Int]
+sep [] = []
+sep (x:xs) = digitToInt x : sep xs
 
 
 -- type input\FindDigits.txt | stack runghc FindDigits.hs
