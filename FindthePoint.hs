6d9fa71db12a9f4979b82c93af21aa9b9c4e692f
tbird <tbird@jp.ibm.com>
2017-09-19 13:33:01 +0900

    一応完成 close #13

--- FindthePoint.hs
+++ FindthePoint.hs
@@ -2,12 +2,30 @@ main :: IO()
 main = do
     _:input<- lines <$> getContents
     -- ["0 0 1 1","1 1 2 2"]
-    print $ map words input
-    -- [["0","0","1","1"],["1","1","2","2"]]
+    mapM_ (putStrLn . f . calc . findpoint) input
+    -- [[2,2],[3,3]]
 
+-- |
+-- >>> f [2,2] 
+-- "2 2"
+
+f :: Show a => [a] -> String
+f [x,y] = show x ++ " " ++ show y
+
+-- |
+-- >>> findpoint "0 0 1 1"
+-- [0,0,1,1]
+findpoint :: String -> [Int]
+findpoint x = map string2Int $ words x
+
+-- |
+-- >>> calc [1,1,2,2]
+-- [3,3]
+calc :: Num a => [a] -> [a]
+calc [px,py,qx,qy] = [2*qx - px , 2*qy - py]
 
 string2Int :: String -> Int
 string2Int = read
 
-    -- https://www.hackerrank.com/challenges/find-point
+-- https://www.hackerrank.com/challenges/find-point
 -- type input\FindthePoint.txt | stack runghc FindthePoint.hs
