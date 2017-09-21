664ab89d8ef79445be41f8f3a72a84d73a9b01f1
tbird <tbird@jp.ibm.com>
2017-09-21 11:26:03 +0900

    wip:ゼロディバイドになる

--- DifferentWays.hs
+++ DifferentWays.hs
@@ -9,16 +9,22 @@ main = do
     let ans = map (\(n:k:_) -> count (read n) (read k)) temp
     mapM_ print ans
 
- 
+fact :: (Enum a, Eq a,Num a) => a -> a
+fact 0 = 1
+fact n = product [1..n]
+
+comb :: Integral a => a -> a -> a
+comb n k = fact n `div` (fact k * fact (n - k))
+
 countMemo :: Int -> Int -> Int
 countMemo = memoize2 count    
-
 
 count :: Int -> Int -> Int
 count n k 
    | k == 0 = 1
    | k == n  = 1
-   | otherwise = countMemo (n -1) (k - 1) + countMemo (n -1) k
+   | otherwise = (comb (n - 1) (k - 1) + comb (n - 1) k) `mod` 100000007
+--   | otherwise = countMemo (n -1) (k - 1) + countMemo (n -1) k
 
 -- https://www.hackerrank.com/challenges/different-ways-fp
 -- type input\DifferentWays.txt | stack runghc DifferentWays.hs
