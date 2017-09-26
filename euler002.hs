3e11fd40f9fa9d7e75f78435cc7e6291190f42c9
tbird <tbird@jp.ibm.com>
2017-09-26 13:13:18 +0900

    通常再帰版完成

---
+++ euler002.hs
@@ -0,0 +1,20 @@
+import Data.Function.Memoize
+
+main :: IO()
+main = do
+   _:list <- lines <$> getContents
+   mapM_ (print . f . string2Int) list
+
+f :: Integer -> Integer
+f n = sum $ filter even $ takeWhile (< n) fib
+
+-- fibMemo = memoize fib
+
+fib :: [Integer]   
+fib = 1:2:zipWith (+) fib (tail fib)
+
+string2Int :: String -> Integer
+string2Int = read
+
+--https://www.hackerrank.com/contests/projecteuler/challenges/euler002
+-- type input\euler002.txt | stack runghc euler002.hs
