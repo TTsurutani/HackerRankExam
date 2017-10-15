3ac070af50a936cee40c81ba9577ea2a8f36d816
TTsurutani <tbird@mbj.nifty.com>
2017-10-16 05:58:51 +0900

    積分式で解いたが回答例とは合わない

--- AreaUnderCurves.hs
+++ AreaUnderCurves.hs
@@ -6,22 +6,36 @@ import Text.Printf (printf)
 -- |
 -- >>> solve 1 4 [1,2,3,4,5] [6,7,8,9,10]
 -- [2435300.3,26172951168940.8]
-solve :: Int -> Int -> [Int] -> [Int] -> [Double]
-solve l r a b = [area , volume]
 
-area :: Int -> Int -> [Int] -> [Int] -> Double
-area l r a b = undefined
+--solve :: Int -> Int -> [Int] -> [Int] -> [Double]
+--solve l r a b = [area , volume]
+--solve l r a b = undefined
+
+-- a1/(b1+1)*x^(b1+1)
+-- area :: Int -> Int -> [Int] -> [Int] -> Double
+--area l r a b = areaCalc r a b - areaCalc l a b
+
+areaCalc :: Int -> [Int] -> [Int] -> Double
+areaCalc n as bs = sum (zipWith (f n) as bs)
+
+f :: Int -> Int -> Int -> Double
+f n a b = (ax/(bx+1))*(nx**(bx+1))
+    where
+        ax = fromIntegral a
+        bx = fromIntegral b
+        nx = fromIntegral n
 
 volume :: Int -> Int -> [Int] -> [Int] -> Double
-volume l r a b = undefined
+volume l r a b = areaCalc r a b - areaCalc l a b
 
 
---Input/Output.
+--Input/Output
+{--
 main :: IO ()
 main = getContents >>= mapM_ (printf "%.1f\n"). 
                        (\[a, b, [l, r]] -> solve l r a b). 
                        map (map read. words). lines
-
+--}
 -- https://www.hackerrank.com/challenges/area-under-curves-and-volume-of-revolving-a-curv
 -- type input\AreaUndercurves.txt | stack runghc AreaUndercurves.hs
                        
