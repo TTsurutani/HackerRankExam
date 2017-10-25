eb63e0e766a60844946525e383b1900d2b94e2bb
tbird <tbird@jp.ibm.com>
2017-10-25 15:54:10 +0900

    ベタな方法完成。本家は解けた

--- euler008.hs
+++ euler008.hs
@@ -1,8 +1,40 @@
+{--
+Sample Input 0
+2
+10 5 (n k)
+3675356291
+10 5
+2709360626
 
+Sample Output 0
+3150
+0
+--}
 import Data.Char
+main :: IO()
+main = do 
+    [xs] <- lines <$> getContents
+    print $ solve "13" xs
+
+sep :: String -> String -> [String]
+sep n xs 
+    | length xs == string2Int n = [xs]
+    | otherwise = take (string2Int n) xs : sep n (tail xs)
+-- | 切り出した要素に対して積を計算する
+-- >>> multi "67535"
+-- 3150
 multi :: String -> Int
 multi xs = product $ map digitToInt xs
 
+-- | kと数字をとって、答えを返す
+-- >>> solve "5" "3675356291"
+-- 3150
+solve :: String -> String -> Int
+solve n xs = maximum $ map multi $ sep n xs
+
+string2Int :: String -> Int
+string2Int = read    
+
 
 -- https://www.hackerrank.com/contests/projecteuler/challenges/euler008
--- type input\euler008 | stack runghc euler008.hs
+-- type input\euler008.txt | stack runghc euler008.hs
