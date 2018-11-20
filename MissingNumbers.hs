c6e7d5d6fd67b91e2a4c4aed3bfe181bf1f241f0
tbird <tbird@jp.ibm.com>
2018-11-20 16:32:39 +0900

    なぜか一ケースのみ失敗する（こっちが正当）

--- MissingNumbers.hs
+++ MissingNumbers.hs
@@ -1,13 +1,20 @@
-import Data.List
+import Data.List(sort)
+
 main :: IO()
 main = do
-    [_ , xs , _ , ys ] <- Prelude.map words.lines <$> getContents
-    output $ sort $ nub $ (\\) ys xs
+  [_,a,_,b] <- map words . lines <$> getContents
+  let ans = unwords. sort . solve a $ b
+  putStr ans
 
-output :: [String] -> IO()
-output = Prelude.foldr (\ x -> (>>) (putStr x >> putStr " ")) (putStrLn "") 
--- output [] = putStrLn ""
--- output (x:xs) = putStr x >> putStr " " >> output xs
+solve :: Eq a => [a] -> [a] -> [a]
+solve [] ys = ys
+solve (x:xs) ys = solve xs (filter' x ys)
+
+filter' :: Eq a => a -> [a] -> [a]
+filter' _ [] = []
+filter' x (y:ys) 
+  | x == y = ys
+  | otherwise = y : filter' x ys
 
 -- type input\MissingNumbers.txt | stack runghc MissingNumbers.hs
 -- https://www.hackerrank.com/challenges/missing-numbers-fp
@@ -16,30 +23,3 @@ output = Prelude.foldr (\ x -> (>>) (putStr x >> putStr " ")) (putStrLn "")
 -- 3670 3674 3677 3684 3685 3695 3714 3720
 -- case03 expected timeOutのみ問題。
 -- 2437 2438 2442 2444 2447 2451 2457 2458 2466 2473 2479 2483 2488 2489 2510 2515 2517 2518
-
--- | リストに要素が含まれる数を返す
-elements ::Eq a => a -> [a] -> Int
-elements _ [] = 0
-elements x (y:ys)
-       | x == y    = elements x ys + 1
-       | otherwise = elements x ys
-
--- | リストに含まれる要素を除去する
-delelm :: Eq a => a -> [a] -> [a]
-delelm _ [] = []
-delelm x (y:ys)
-      | x == y    = delelm x ys
-      | otherwise = y:delelm x ys
-
--- | リストをキーとカウントのペアに変換
-pairs :: Eq a => [a] -> [(a,Int)]
-pairs [] = []
-pairs (x:xs) = (x, elements x xs + 1) : pairs (delelm x xs)
-
-
--- | リストBにはあって、Aにはないもののリスト
-func :: Eq a => [a] -> [a] -> [a]
-func [] _ = []
-func (x:xs) (y:ys)
-        | x == y    = func xs ys
-        | otherwise = y : func xs ys 
