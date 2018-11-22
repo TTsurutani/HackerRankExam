62a161dd986f699bc4f660fff1c278894a9f83f9
tbird <tbird@jp.ibm.com>
2018-11-22 12:30:58 +0900

    ちょっと冗長な気はするが成功

--- ListsAndGCD.hs
+++ ListsAndGCD.hs
@@ -1,31 +1,35 @@
-
+import Data.Maybe
 main :: IO()
-main = do
-    _ : list <- lines <$> getContents
-    let tlist = map words list
-    -- [["2","2","3","2","5","3"],["3","2","5","3","11","1"]]
-    let plist = map pairs tlist
-    -- [[(2,2),(3,2),(5,3)],[(3,2),(5,3),(11,1)]]
-    print $ map (map barasu) plist
-    {-- [
-        [[2,2],[3,3],[5,5,5]],
-        [[3,3],[5,5,5],[11]],
-        [[2,2],[3,3,3],[5,5,5,5],[7,7,7,7,7,7],[19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19]],[[3,3,3,3,3,3,3,3,3,3],[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]]]
-    --}
-barasu :: (Int,Int) -> [Int]
-barasu (x,y) = replicate y x
+main = do 
+  _:list <- map (map read'.words).lines <$> getContents
+  putStrLn . unwords . unpair . gcd' . map pairing $ list
 
+read'::String->Int
+read' = read  
 
+-- フラットなリストを指数とのペアに置換
+pairing :: [Int] -> [(Int,Int)]
+pairing [] = []
+pairing (x:y:xs) = (x,y) : pairing xs
 
+-- 二つの数字のGCDを求める
+wgcd :: [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
+wgcd [] _ = []
+wgcd ((k,v):xs) ys 
+  | isNothing x = wgcd xs ys
+  | otherwise = (k , min v (fromJust x)) : wgcd xs ys
+  where
+    x = lookup k ys
 
+-- 複数の数値を頭から順にGCDしていく
+gcd' :: [[(Int,Int)]] -> [(Int,Int)]
+gcd' [x] = x
+gcd' (x:y:ys) = gcd' (wgcd x y : ys)
 
-
-pairs :: [String] -> [(Int,Int)]
-pairs [] = []
-pairs (x:y:xs) = (read x, read y) : pairs xs
-
-depairs :: [(Int,Int)] -> String
-depairs = undefined
+-- pairを回答形式に戻す
+unpair :: [(Int,Int)] -> [String]
+unpair [] = []
+unpair (x:xs) = show (fst x) : show (snd x) : unpair xs
 
 -- type input\ListsAndGCD.txt | stack runghc ListsAndGCD.hs
 -- https://www.hackerrank.com/challenges/lists-and-gcd
