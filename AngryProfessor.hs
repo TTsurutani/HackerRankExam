bc78d589b01ff8ece3f89ae8057e6ee20620898c
tbird <tbird@jp.ibm.com>
2018-11-22 14:52:57 +0900

    不等号がややこしかったが問題は比較的単純で成功

--- AngryProfessor.hs
+++ AngryProfessor.hs
@@ -1,14 +1,17 @@
 main :: IO()
 main = do 
-    _ : input <- lines <$> getContents
-    print  $ map (map string2Int.words) $ pair input
+  _:list <- map (map (read::String->Int) . words) . lines <$> getContents
+  -- [[4,3],[-1,-3,4,2],[4,2],[0,-1,2,1]]
+  mapM_ putStrLn . decode $ list
 
-pair :: [a] -> [[a]]
-pair [] = []    
-pair (x:y:xs) = [x,y] : pair xs
+decode :: [[Int]] -> [String]
+decode [] = []
+decode ([_,k]:xs:rest) = f k xs : decode rest
 
-string2Int :: String -> Int
-string2Int = read
+f :: (Num a, Ord a) => Int -> [a] -> String
+f k xs = if f' then "YES" else "NO"
+           where
+             f' = (k >=) . length . filter (>= 0) $ xs
 
 -- type input\AngryProfessor.txt | stack runghc AngryProfessor.hs
 -- https://www.hackerrank.com/challenges/angry-professor/problem
