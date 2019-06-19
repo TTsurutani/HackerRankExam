af811307f6d66e6729f99e50b36ee6bf32546f5c
tbird <tbird@mbj.nifty.com>
2019-06-20 05:37:10 +0900

    euler011理解のため整形

--- euler011.hs
+++ euler011.hs
@@ -1,11 +1,22 @@
 import Control.Arrow
 import Data.Array
 
+main = print 
+       . maximum 
+       . prods 
+       . input =<< getContents
+
 input :: String -> Array (Int,Int) Int
-input = listArray ((1,1),(20,20)) . map read . words
+input = listArray ((1,1),(20,20))
+        . map read . words
 
-senses = [(+1) *** id,(+1) *** (+1), id *** (+1), (+1) *** (\n -> n - 1)]
+senses :: [(Int, Int) -> (Int, Int)]
+senses = [(+1) *** id,
+          (+1) *** (+1), 
+          id *** (+1), 
+          (+1) *** (\n -> n - 1)]
 
+inArray :: Ix a => Array a e -> a -> Bool          
 inArray a i = inRange (bounds a) i
 
 prods :: Array (Int, Int) Int -> [Int]
@@ -14,4 +25,3 @@ prods a = [product xs | i <- range $ bounds a,
                         let is = take 4 $ iterate s i,
                         all (inArray a) is,
                         let xs = map (a!) is]
-main = print . maximum . prods . input =<< getContents
