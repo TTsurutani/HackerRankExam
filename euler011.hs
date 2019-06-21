ad6f1f77e914f0beee7902622a56ccc2a7da05c3
tbird <tbird@mbj.nifty.com>
2019-06-21 09:25:57 +0900

    euler011一旦は理解

--- euler011.hs
+++ euler011.hs
@@ -1,5 +1,5 @@
-import Control.Arrow
-import Data.Array
+-- import Control.Arrow
+import Data.Array.IArray
 
 main :: IO ()
 main = print 
@@ -7,21 +7,26 @@ main = print
        . prods 
        . input =<< getContents
 
--- 入力をArrayに突っ込む       
+-- 入力をArrayに
 input :: String -> Array (Int,Int) Int
 input = listArray ((1,1),(20,20))
         . map read . words
 
--- 縦に、右肩下がりに、横に、右肩あがりの座標に変換する関数のリスト
+-- 縦に、右肩下がりに、横に、右肩あがりに座標に変換する関数のリスト
 senses :: [(Int, Int) -> (Int, Int)]
+senses = [\(x,y)->(x+1,y),
+          \(x,y)->(x+1,y+1),
+          \(x,y)->(x,y+1),
+          \(x,y)->(x+1,y-1) ]
+{- 
 senses = [(+1) *** id,  -- first (+1)
           (+1) *** (+1), 
           id *** (+1),  -- second (+1)
           (+1) *** (\n -> n - 1)]
-
---- Arrayとインデックスをとって範囲内かを返す          
-inArray :: Ix a => Array a e -> a -> Bool          
-inArray a i = inRange (bounds a) i
+ -}
+-- インデックスがArrayの範囲内か否かを返す          
+inArray :: Ix i => Array i e -> i -> Bool          
+inArray a = inRange (bounds a)
 
 -- 配列をとって、条件に合致した要素の積のリストを返す
 prods :: Array (Int, Int) Int -> [Int]
