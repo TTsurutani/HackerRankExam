e2e7f20d78eeae63fb17d84dbb8b0c0bbd2ef39f
tbird <tbird@jp.ibm.com>
2019-06-26 16:48:32 +0900

    euler014解析暫定

--- euler014.hs
+++ euler014.hs
@@ -13,6 +13,10 @@ f n
    -}
 
 import Data.List   
+import Control.Parallel
+import Data.Word
+import Data.Array
+import Data.Ord (comparing)
 
 ans :: Integer
 ans = j 1000000
@@ -23,11 +27,7 @@ ans = j 1000000
 j :: Integer -> Integer
 j n = fst $ foldl' h (1,1) [2..n-1]
 
--- gの第二引数は与えられた値の深さを計算する
--- つまり第二引数はリスト要素nとその深さのペアを返すことになる
--- 結果的に新しいｎと、これまでの最深のペアとを比較して深い方を返す関数
-h :: (Integer, Int) -> Integer -> (Integer, Int)
-h x n = g x (n, f 1 n)
+
 
 -- ペアのsndを比較して大きい方のペアを戻す（勝ち抜きのため）
 g :: Ord a => (a1, a) -> (a1, a) -> (a1, a)
@@ -42,6 +42,39 @@ f k 1 = k
 f k n = f (k+1) 
         $ if even n then div n 2 else 3*n + 1   
 
+collatzLen :: Int -> Word32 -> Int
+collatzLen c 1 = c
+collatzLen c n = collatzLen (c+1) $ if n `mod` 2 == 0 then n `div` 2 else 3*n+1
+-------------------------------------------------------------
+
+h :: (Integer, Int) -> Integer -> (Integer, Int)
+h x n = g x (n, f 1 n)
+
+pmax :: (Int, Word32) -> Word32 -> (Int, Word32)
+pmax x n = x `max` (collatzLen 1 n, n)
+
+solve :: [Word32] -> (Int, Word32)
+solve  = foldl pmax (1,1) 
+
+{- 
+main :: IO()
+main = print soln
+    where
+        s1 = solve [2..500000]
+        s2 = solve [500001..1000000]
+        soln = s2 `par` (s1 `pseq` max s1 s2)
+-- par , pseqとも Controll.Parallelの関数
+ -}
 
 
+syrs n = 
+    a
+    where 
+    a = listArray (1,n) $ 0 : map syr [2..n]
+    syr x = 
+        if y <= n then 1 + a ! y else 1 + syr y
+        where 
+        y = if even x then x `div` 2 else 3 * x + 1
 
+main = 
+    print . maximumBy (comparing snd) . assocs . syrs $ 1000000
