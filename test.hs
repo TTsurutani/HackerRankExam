9331d8a4dba721d9d3b040d3e505648218e8a7ec
tbird <tbird@mbj.nifty.com>
2023-06-08 21:04:41 +0900

    sortOnのお試し用

---
+++ test.hs
@@ -0,0 +1,15 @@
+
+import Data.List(sortOn)
+
+depth :: Char -> String -> Int
+depth _ [] = 0
+depth y (x:xs) 
+  | x == y = depth y xs  + 1
+  | otherwise = depth y xs 
+
+head' :: [String] -> String
+head' [] = "Not Found"
+head' xs = head xs
+
+section :: [String] -> String
+section = head' . (sortOn (depth '.'))
