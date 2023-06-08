3efa73a61fa7aae7259a38f18c3f21c0e946eff8
tbird <tbird@mbj.nifty.com>
2023-06-08 21:17:06 +0900

    format他

--- test.hs
+++ test.hs
@@ -1,15 +1,14 @@
-
-import Data.List(sortOn)
+import Data.List (sortOn)
 
 depth :: Char -> String -> Int
 depth _ [] = 0
-depth y (x:xs) 
-  | x == y = depth y xs  + 1
-  | otherwise = depth y xs 
+depth y (x : xs)
+  | x == y = depth y xs + 1
+  | otherwise = depth y xs
 
 head' :: [String] -> String
 head' [] = "Not Found"
 head' xs = head xs
 
 section :: [String] -> String
-section = head' . (sortOn (depth '.'))
+section = head' . sortOn (depth '.')
